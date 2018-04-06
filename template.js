44422018838e134d47580a74a409da0c87b38fdd
C8-Developers <37783088+C8-Developers@users.noreply.github.com>
2018-04-05 19:36:19 -0500

    Fixed Errors

--- template.js
+++ template.js
@@ -1,3 +1,5 @@
+ModPE.log(0)
+menuBtn();
 /*
 Made By The C8 Development Team
 */
@@ -7,7 +9,7 @@ var modAuthor = "C8 Developers";
 var achievements = "Achievement Test";
 var play = "Play Test";
 var store = "Store Test";
-var settings = "Settings/ Options Test"
+var settings = "Settings","Options Test"
 //The Following Is Premade For U To Make The Copyright
 var copyright = "+modName";
 /*
@@ -35,142 +37,161 @@ var button10 = Button;
 var GUI;
 var menu;
 var exitUI;
+
 function dip2px(dips){
-var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    return Math.ceil(dips * ctx.getResources().getDisplayMetrics().density);
 }
-function newLevel(){
-var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
-try{
-var layout = new android.widget.LinearLayout(ctx);
-layout.setOrientation(1);
-var menuBtn = new android.widget.Button(ctx);
-menuBtn.setText('C8 Template');
-menuBtn.setOnClickListener(new android.view.View.OnClickListener({
-onClick: function(viewarg){
-mainMenu();
-exit();
-}
-}));
-layout.addView(menuBtn);
-GUI = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
-GUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
-GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.BOTTOM, 10, 20);
-}catch(err){
-print('An error occured: ' + err);
-}
-}}));
-}
- function mainMenu(){
-var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
-try{
-var menuLayout = new android.widget.LinearLayout(ctx);
-var menuScroll = new android.widget.ScrollView(ctx);
-var menuLayout1 = new android.widget.LinearLayout(ctx);
-menuLayout.setOrientation(1);
-menuLayout1.setOrientation(1);
-menuScroll.addView(menuLayout);
-menuLayout1.addView(menuScroll);
-var button = new android.widget.Button(ctx);
-button.setText('+button1+');
-button.setOnClickListener(new android.view.View.OnClickListener({
-onClick: function(viewarg){
-    //Where Ur Code Goes
-    }
-    }));
-    menuLayout.addView(button);
-    
-    var button = new android.widget.Button(ctx);
-    button.setText('+button2+');
-    button.setOnClickListener(new android.view.View.OnClickListener({
-    onClick: function(viewarg){
-        //Where Ur Code Goes
-        }
-        }));
-        
-        var button = new android.widget.Button(ctx);
-        button.setText('+button3+');
-        button.setOnClickListener(new android.view.View.OnClickListener({
-        onClick: function(viewarg){
-            //Where Ur Code Goes
-            }
+
+function menuBtn(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
+        try{
+            var layout = new android.widget.LinearLayout(ctx);
+            layout.setOrientation(1);
+
+            var menuBtn = new android.widget.Button(ctx);
+            menuBtn.setText("+modName+""+modVersion+");
+            menuBtn.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+                    mainMenu();
+                }
             }));
-            
+            layout.addView(menuBtn);
+
+            GUI = new android.widget.PopupWindow(layout, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT, android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+            GUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+            GUI.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.BOTTOM, 0, 0);
+        }catch(err){
+            print("An error occured: " + err);
+        }
+    }}));
+}
+
+function mainMenu(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
+        try{
+            var menuLayout = new android.widget.LinearLayout(ctx);
+            var menuScroll = new android.widget.ScrollView(ctx);
+            var menuLayout1 = new android.widget.LinearLayout(ctx);
+            menuLayout.setOrientation(1);
+            menuLayout1.setOrientation(1);
+
+            menuScroll.addView(menuLayout);
+            menuLayout1.addView(menuScroll);
+
             var button = new android.widget.Button(ctx);
-            button.setText('+button5+');
+            button.setText("EXIT");
+            button.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+                    menu.dismiss();
+                    menu1.dismiss();
+                }
+            }));
+            menuLayout.addView(button);
+
+            var button = new android.widget.Button(ctx);
+            button.setText('+button1+');
             button.setOnClickListener(new android.view.View.OnClickListener({
             onClick: function(viewarg){
                 //Where Ur Code Goes
                 }
                 }));
+                menuLayout.addView(button);
                 
                 var button = new android.widget.Button(ctx);
-                button.setText('+button6+');
+                button.setText('+button2+');
                 button.setOnClickListener(new android.view.View.OnClickListener({
                 onClick: function(viewarg){
                     //Where Ur Code Goes
                     }
                     }));
-    /*
-    The Following Edits The MCPE Main Page
-    */
-    ModPE.langEdit("menu.copyright","+copyright+")
-    ModPE.langEdit("menu.play","+play+")
-    ModPE.langEdit("menu.settings","+settings+")
-    ModPE.langEdit("menu.store","+store+")
-    ModPE.langEdit("menu.achievements","+achievements")
-    
-    
-    
-    
-    
-    menu = new android.widget.PopupWindow(menuLayout1, ctx.getWindowManager().getDefaultDisplay().getWidth()/2, ctx.getWindowManager().getDefaultDisplay().getHeight());
-    menu.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.BLACK));
-    menu.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
-    }catch(error){
-    print('An error occured: ' + error);
-    }
+                    
+                    var button = new android.widget.Button(ctx);
+                    button.setText('+button3+');
+                    button.setOnClickListener(new android.view.View.OnClickListener({
+                    onClick: function(viewarg){
+                        //Where Ur Code Goes
+                        }
+                        }));
+                        
+                        var button = new android.widget.Button(ctx);
+                        button.setText('+button5+');
+                        button.setOnClickListener(new android.view.View.OnClickListener({
+                        onClick: function(viewarg){
+                            //Where Ur Code Goes
+                            }
+                            }));
+                            
+                            var button = new android.widget.Button(ctx);
+                            button.setText('+button6+');
+                            button.setOnClickListener(new android.view.View.OnClickListener({
+                            onClick: function(viewarg){
+                                //Where Ur Code Goes
+                                }
+                                }));
+                /*
+                The Following Edits The MCPE Main Page
+                */
+                ModPE.langEdit("menu.copyright","+copyright+")
+                ModPE.langEdit("menu.play","+play+")
+                ModPE.langEdit("menu.settings","+settings+")
+                ModPE.langEdit("menu.store","+store+")
+                ModPE.langEdit("menu.achievements","+achievements")
+                
+
+            menu = new android.widget.PopupWindow(menuLayout1, ctx.getWindowManager().getDefaultDisplay().getWidth()/4, ctx.getWindowManager().getDefaultDisplay().getHeight());
+            menu.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.BLACK));
+            menu.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
+        }catch(error){
+            print("An error occured: " + error);
+        }
     }}));
-    }
-    function exit(){
-    var ctxe = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
-    ctxe.runOnUiThread(new java.lang.Runnable({ run: function(){
-    try{
-    var xLayout = new android.widget.LinearLayout(ctxe);
-    var xButton = new android.widget.Button(ctxe);
-    xButton.setText('X');
-    xButton.setTextColor(android.graphics.Color.WHITE);
-    xButton.setOnClickListener(new android.view.View.OnClickListener({
-    onClick: function(viewarg){
-    exitUI.dismiss();
-    menu.dismiss();
-    }
-    }));
-    xLayout.addView(xButton);
-    exitUI = new android.widget.PopupWindow(xLayout, dip2px(40), dip2px(40));
-    exitUI.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
-    exitUI.showAtLocation(ctxe.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 0);
-    }catch(exception){
-    print(exception);
-    }
-    }}));
-    }
-    function leaveGame(){
+}
+function mainMenu(){
     var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
     ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
-    if(GUI != null){
-    GUI.dismiss();
-    GUI = null;
-    }
-    if(menu != null){
-    menu.dismiss();
-    menu = null;
-    }
-    if(exitUI != null){
-    exitUI.dismiss();
-    exitUI = null;
-    }
+        try{
+            var menuLayout1 = new android.widget.LinearLayout(ctx);
+            var menuScroll1 = new android.widget.ScrollView(ctx);
+            var menuLayout11 = new android.widget.LinearLayout(ctx);
+            menuLayout1.setOrientation(1);
+            menuLayout11.setOrientation(1);
+
+            menuScroll1.addView(menuLayout1);
+            menuLayout11.addView(menuScroll1);
+
+            var button = new android.widget.Button(ctx);
+            button.setText("Button");
+            button.setOnClickListener(new android.view.View.OnClickListener({
+                onClick: function(viewarg){
+                    //Your Code
+                }
+            }));
+            menuLayout1.addView(button);
+
+            //Add more buttons in this section
+
+            menu1 = new android.widget.PopupWindow(menuLayout11, ctx.getWindowManager().getDefaultDisplay().getWidth()/4, ctx.getWindowManager().getDefaultDisplay().getHeight());
+            menu1.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.YELLOW));
+            menu1.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.LEFT | android.view.Gravity.TOP, 0, 0);
+        }catch(error){
+            print("An error occured: " + error);
+        }
     }}));
-    }
+}
+function leaveGame(){
+    var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+    ctx.runOnUiThread(new java.lang.Runnable({ run: function(){
+        if(GUI != null){
+            GUI.dismiss();
+            GUI = null;
+        }
+        if(menu != null){
+            menu.dismiss();
+            menu = null;
+        }
+        
+    }}));
+}
